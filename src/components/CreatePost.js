1476b1bab2c69cbbe948be75edcf745216ca2339
oelglad <omnia_elglad@aol.com>
2019-12-12 14:18:43 -0500

    all

---
+++ src/components/CreatePost.js
@@ -0,0 +1,27 @@
+import React from 'react';
+const CreatePost = (props) => {
+    return (
+        <form onSubmit={props.postSubmit}
+        className="post-form"
+        >
+        <input
+        type="text"
+        name="title"
+        value={props.title}
+        placeholder="Title!"
+        onChange={props.onContentChange}
+        />
+        <textarea
+          name="content"
+          value={props.content}
+          placeholder="POST IT"
+          onChange={props.onContentChange}
+          rows="4"
+        />
+        <input type="submit" value="POST" />
+      </form>
+    )
+}
+
+export default CreatePost;
+
