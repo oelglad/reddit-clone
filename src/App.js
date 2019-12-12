684c9bc7b4fa6ddf6832c3788dd1d21d7335a1e9
oelglad <omnia_elglad@aol.com>
2019-12-12 11:02:14 -0500

    [Src] Initial csaffolding in place.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ""
+    }
+  }
+  onChange =(e) => {
+    const content = e.target.value;
+    this.setState({
+      content
+    })
+  }
+  postSubmit
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.postSubmit}>
+          <input
+            type="textarea"
+            name="content"
+            placeholder="POST IT"
+            value={this.state.content}
+            onChange={this.onChange}
+          />
+          <input type="submit" value="POST" />
+        </form>
+      </div>
+    );
+  }
 }
-
 export default App;
